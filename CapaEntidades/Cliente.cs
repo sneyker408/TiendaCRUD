270b41412fe3e8f6a4101343a7c98deea04a97e9
HumberAlas <126435236+HumberAlas@users.noreply.github.com>
2024-04-18 17:34:35 -0600

    Update Cliente.cs

--- CapaEntidades/Cliente.cs
+++ CapaEntidades/Cliente.cs
@@ -5,7 +5,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+       
 namespace CapaEntidades
 {
     public class Cliente

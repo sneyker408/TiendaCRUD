beccb30ab428f222701278642bd896d86b7d3d06
zFlixT <126367920+zFlixT@users.noreply.github.com>
2024-04-18 17:43:36 -0600

    Update Cliente.cs
    Comit 1.2

--- CapaEntidades/Cliente.cs
+++ CapaEntidades/Cliente.cs
@@ -13,31 +13,25 @@ namespace CapaEntidades
     {     
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-
         public int ClienteID { get; set; }
 
         [Required(ErrorMessage = "El nombre del cliente es obligatorio.")]
         [StringLength(100, ErrorMessage = "El nombre del cliente no puede tener más de 100 caracteres.")]
-           
         public string Nombre { get; set; }
 
         [Required(ErrorMessage = "El apellido del cliente es obligatorio.")]
-        [StringLength(100, ErrorMessage = "El apellido del cliente no puede tener más de 100 caracteres.")]
-           
+        [StringLength(100, ErrorMessage = "El apellido del cliente no puede tener más de 100 caracteres.")]   
         public string Apellido { get; set; }
         
         [Required(ErrorMessage = "El correo electrónico del cliente es obligatorio.")]
         [StringLength(100, ErrorMessage = "El correo electrónico del cliente no puede tener más de 100 caracteres.")]
-        [EmailAddress(ErrorMessage = "El correo electrónico del cliente no es válido.")]
-           
+        [EmailAddress(ErrorMessage = "El correo electrónico del cliente no es válido.")]   
         public string CorreoElectronico { get; set; }
 
-        [StringLength(255, ErrorMessage = "La dirección del cliente no puede tener más de 255 caracteres.")]
-           
+        [StringLength(255, ErrorMessage = "La dirección del cliente no puede tener más de 255 caracteres.")] 
         public string Direccion { get; set; }
 
         [StringLength(20, ErrorMessage = "El número de teléfono del cliente no puede tener más de 20 caracteres.")]
-           
         public string NumeroTelefono { get; set; }
     }      
 }

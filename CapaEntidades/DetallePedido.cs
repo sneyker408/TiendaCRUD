504b7a222a2192c4d72320b47d6d56012612216f
zFlixT <126367920+zFlixT@users.noreply.github.com>
2024-04-18 17:44:37 -0600

    Update DetallePedido.cs
    Comit 1.4

--- CapaEntidades/DetallePedido.cs
+++ CapaEntidades/DetallePedido.cs
@@ -12,7 +12,6 @@ namespace CapaEntidades
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-
         public int DetallePedidoID { get; set; }
 
         [Required(ErrorMessage = "El pedido es obligatorio.")]

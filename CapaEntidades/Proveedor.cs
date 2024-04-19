57804d8d6aac7d4dfda145bee7dce97f2caf49fa
sneyker_408 <100097465+sneyker408@users.noreply.github.com>
2024-04-18 18:11:25 -0600

    Update Proveedor.cs

--- CapaEntidades/Proveedor.cs
+++ CapaEntidades/Proveedor.cs
@@ -12,7 +12,6 @@ namespace CapaEntidades
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-
         public int ProveedorID { get; set; }
 
         [Required(ErrorMessage = "El nombre del proveedor es obligatorio.")]
